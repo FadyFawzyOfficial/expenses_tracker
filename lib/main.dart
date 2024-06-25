218f784ee8da0568468e4ca66bb4c583d957759e
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-06-25 10:48:20 +0300

    1. Start building main screen of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+import 'expenses.dart';
+
+void main() => runApp(const ExpensesTracker());
+
+class ExpensesTracker extends StatelessWidget {
+  const ExpensesTracker({super.key});
+
+  @override
+  Widget build(context) {
+    return const MaterialApp(
+      home: Expenses(),
+    );
+  }
+}
