369c6f027cf02f8a78762c3f9158161f6e4d6cef
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2024-07-02 21:51:50 +0300

    25. Get started with theming & Set color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,24 @@ import 'widgets/expenses.dart';
 
 void main() => runApp(const ExpensesTracker());
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 class ExpensesTracker extends StatelessWidget {
   const ExpensesTracker({super.key});
 
   @override
   Widget build(context) {
-    return const MaterialApp(
-      home: Expenses(),
+    return MaterialApp(
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      home: const Expenses(),
     );
   }
 }
